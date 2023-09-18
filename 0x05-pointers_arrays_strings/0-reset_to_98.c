c5f028abb5d79f47ff60f257e446a08fc5a741e3
huighlet <huighlet.zowonu@htu.edu.gh>
2023-09-18 07:00:51 -0700

    pointers and arrays updated...

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 #include "main.h"  /* This if included function header*/
-void reset_to_98(int *n)
+void reset_to_98(int *n)/*reset to 98 function*/
 {
 	*n = 98;/*update the value at the memory location pointed by n to 98*/
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-void reset_to_98(int *);
+void reset_to_98(int *);/*reset to 98 function*/
 #endif /* MAIN_H */
