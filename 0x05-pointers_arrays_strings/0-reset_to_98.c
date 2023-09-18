357bc698ac0be77db78d8fb3e44c4c2297603c63
huighlet <huighlet.zowonu@htu.edu.gh>
2023-09-18 07:40:08 -0700

    pointers and arrays updated........

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,7 +5,7 @@
  *
  * Description: This function takes a pointer to an integer as a parameter
  * and updates the it points to 98.
- * /
+ */
 void reset_to_98(int *n)  
 {
 	*n = 98;/*update the value at the memory location pointed by n to 98*/
