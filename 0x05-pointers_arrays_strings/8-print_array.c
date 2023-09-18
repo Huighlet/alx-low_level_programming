521285472f88ea35bd97618a420a4bfb27335843
huighlet <huighlet.zowonu@htu.edu.gh>
2023-09-18 12:26:04 -0700

    array and strings 9.0

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,16 +9,16 @@ void print_array(int *a, int n)
 {
 	if (n <= 0)
 	{
-		printf("\n");
-		return;
+	printf("\n");
+	return;
 	}
-	for (int i = 0; i < n; i++)
+	for(int i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
+	printf("%d", a[i]);
 
-		if (i < n - 1)
+	if (i < n - 1)
 		{
-			printf(", ");
+		printf(", ");
 		}
 	}
 	printf("\n");
