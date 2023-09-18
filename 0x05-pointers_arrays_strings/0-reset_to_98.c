23ae97fbc5f2e32f61dca6524957175bae61b121
huighlet <huighlet.zowonu@htu.edu.gh>
2023-09-18 07:15:16 -0700

    pointers and arrays updated.....

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"  /** This if included function header*/
+#include "main.h"/*reset_to_98 - Updates the value pointed to by a pointer to 98.*/
 void reset_to_98(int *n)/*reset to 98 function*/
 {
 	*n = 98;/*update the value at the memory location pointed by n to 98*/
