2770d203f7c855648e9fe02a7ab943dabe87d61d
huighlet <huighlet.zowonu@htu.edu.gh>
2023-09-22 16:56:25 -0700

    advanced

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,31 +1,22 @@
-#include <unistd.h>
+#include "main.h"
 
-void print_digit(int n)
-{
-	char digit = n + '0';
-	write(1, &digit, 1);
-}
-
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ * Description: This function takes an integer 'n' as a parameter and
+ *      * prints it to the standard output using only the _putchar function.
+ *       * It does not use long, arrays, pointers, or hard-coded special values.
+ *        */
 void print_number(int n)
 {
 	if (n < 0)
 	{
-		write(1, "-", 1);
-		n = -n;
+		_putchar('-'); /* Print the minus sign for negative numbers */
+		n = -n; /* Make 'n' positive for easier printing */
 	}
 
-	if (n >= 10)
-	{
-		print_number(n / 10);
-	}
+	if (n / 10 != 0)
+		print_number(n / 10); /* Recursively print the digits before the last one */
 
-	print_digit(n % 10);
-}
-
-int main()
-{
-	int number = 12345;
-	print_number(number);
-	write(1, "\n", 1);
-	return 0;
+	_putchar((n % 10) + '0'); /* Print the last digit as a character */
 }
