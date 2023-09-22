a47c22c7079dd9092585536a3dfeed7c31959f9f
huighlet <huighlet.zowonu@htu.edu.gh>
2023-09-22 16:39:36 -0700

    0x06 arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,31 @@
-#include <stdio.h>
-
+/**
+ *  rot13 - Encodes a string using ROT13.
+ *  @str: The string to be encoded.
+ *  Return: A pointer to the encoded string.
+ *  Description: This function takes a string 'str' as a parameter and
+ *  encodes it using the ROT13 algorithm, which shifts each letter by 13
+ *  positions in the alphabet. The function uses two loops and only one 'if'
+ *  statement in the code, without using 'switch' or ternary operations.
+ */
 char *rot13(char *str)
 {
-	char *ptr = str;
+	char *result = str;
 
-	while (*ptr)
+	while (*str)
 	{
-		if ((*ptr >= 'A' && *ptr <= 'Z') || (*ptr >= 'a' && *ptr <= 'z'))
-		{
-			char base = (*ptr >= 'A' && *ptr <= 'Z') ? 'A' : 'a';
-			*ptr = ((*ptr - base + 13) % 26) + base;
-		}
-		ptr++;
+		char is_lower = (*str >= 'a' && *str <= 'z');
+		char is_upper = (*str >= 'A' && *str <= 'Z');
+		char shift = (is_lower || is_upper) ? 13 : 0;
+
+		if (is_lower && (*str + shift) > 'z')
+			*str = *str - 13;
+		else if (is_upper && (*str + shift) > 'Z')
+			*str = *str - 13;
+
+		*str += shift;
+
+		str++;
 	}
 
-	return str;
-}
-
-int main()
-{
-	char str[] = "Hello, ROT13!";
-	printf("Original string: %s\n", str);
-
-	rot13(str);
-	printf("ROT13 encoded string: %s\n", str);
-
-	return 0;
+	return result;
 }
