cba1e232b79105edb0f93d44cf9c074b35c2e3e5
huighlet <huighlet.zowonu@htu.edu.gh>
2023-09-22 16:59:40 -0700

    advanced

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: This is a sample C program that demonstrates
+ * how to access and modify elements of an array using a pointer.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int a[5] = {98, 97, 99, 100, 101};
-	int *p = a;
+	int a[5] = {0, 1, 2, 3, 4};
+	int *p;
 
+	p = a;
+
+	/* Add one line of code here */
 	*(p + 2) = 98;
-	printf("a[2] = %d\n", *(p + 2));
+
+	printf("a[2] = %d\n", a[2]);
+
 	return (0);
 }
