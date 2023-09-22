ac9004a0e12cec55d9d17c9642ccdff21450bbcb
huighlet <huighlet.zowonu@htu.edu.gh>
2023-09-22 16:07:08 -0700

    0x06 arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,32 +1,30 @@
-#include <stdio.h>
-
-char *_strncpy(char *dest, const char *src, int n)
+/**
+ * _strncpy - Copies a string up to a specified number of characters.
+ * @dest: The destination string.
+ * @src: The source string to copy from.
+ * @n: The maximum number of characters to copy from src.
+ * Return: A pointer to the beginning of the copied string (dest).
+ */
+char *_strncpy(char *dest, char *src, int n)
 {
-	char *original_dest = dest;
+	char *result = dest; /* Store the original pointer to dest */
 
-	while (*src && n > 0)
+	/* Copy characters from src to dest, up to n bytes or until src ends */
+	while (*src != '\0' && n > 0)
 	{
-		*dest++ = *src++;
+		*dest = *src;
+		dest++;
+		src++;
 		n--;
 	}
 
+	/* If n is greater than the length of src, pad dest with null characters */
 	while (n > 0)
 	{
-		*dest++ = '\0';
+		*dest = '\0';
+		dest++;
 		n--;
 	}
 
-	return original_dest;
-}
-
-int main()
-{
-	char dest[50];
-	const char src[] = "Hello, world!";
-
-	_strncpy(dest, src, 5);
-
-	printf("Copied string: %s\n", dest);
-
-	return 0;
+	return result; /* Return a pointer to the beginning of the copied string */
 }
