a78dce4b0c51dca57a33c1c035514c1996d92c9a
huighlet <huighlet.zowonu@htu.edu.gh>
2023-09-23 13:37:09 -0700

    0x06 strings and arrays + advanced

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,23 +2,23 @@
 
 int main(void)
 {
-	/* Add one line of code here */
-	int a[5] = {0, 1, 2, 3, 4};
+	int n;
+	int a[5];
 	int *p;
 
-	p = a;
-
-	int result = *(p + 2);
-
-	write(1, "a[2] = ", 7);
-	write(1, &result, 1);
-	write(1, "\n", 1);
-
-	/**
-	 * Description: This is a sample C program that demonstrates
-	 * how to access and print the value of an element in an array
-	 * using a pointer. It prints "a[2] = 98" followed by a new line.
+	a[2] = 1024;
+	p = &n;
+	/*
+	 *  write your line of code here...
+	 *  Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
 	 */
-
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
+
