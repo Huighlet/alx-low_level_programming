afad6bb3cfea303d1738e0e72dc6ece70ea79222
huighlet <huighlet.zowonu@htu.edu.gh>
2023-09-22 17:35:27 -0700

    advanced

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * print_buffer - Prints the content of a buffer in a specified format.
+ * @b: The buffer to be printed.
+ * @size: The size of the buffer.
+ * Description: This function prints the content of 'size' bytes of the buffer 'b'
+ * in a specific format. Each line shows the position of the first byte in hexadecimal,
+ * followed by the hexadecimal content (2 bytes at a time), and then the character
+ * representation of the bytes. Lines are formatted with 10 bytes per line.
+ */
 void print_buffer(char *b, int size)
 {
 	if (size <= 0)
@@ -8,15 +17,17 @@ void print_buffer(char *b, int size)
 		return;
 	}
 
-	for (int i = 0; i < size; i += 10)
-	{
-		printf("%.8x ", i);
+	int i, j;
 
-		for (int j = 0; j < 10; j++)
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size)
 			{
-				printf("%.2hhx", b[i + j]);
+				printf("%02x", b[i + j]);
 			}
 			else
 			{
@@ -29,16 +40,13 @@ void print_buffer(char *b, int size)
 			}
 		}
 
-		printf(" ");
-
-		for (int j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size)
 			{
-				char c = b[i + j];
-				if (c >= 32 && c <= 126)
+				if (b[i + j] >= 32 && b[i + j] <= 126)
 				{
-					printf("%c", c);
+					printf("%c", b[i + j]);
 				}
 				else
 				{
@@ -49,14 +57,4 @@ void print_buffer(char *b, int size)
 
 		printf("\n");
 	}
-}
-
-int main()
-{
-	char buffer[] = "This is a sample buffer for printing.";
-	int size = sizeof(buffer) - 1;
-
-	print_buffer(buffer, size);
-
-	return 0;
 }
