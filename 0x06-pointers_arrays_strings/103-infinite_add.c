a47cb3c6930c6a546bed5774a9411f6e65af3b8b
huighlet <huighlet.zowonu@htu.edu.gh>
2023-09-22 17:29:39 -0700

    advanced

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,56 +1,58 @@
 #include <stdio.h>
+#include <string.h>
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
 	int carry = 0;
+	int max_len = len1 > len2 ? len1 : len2;
 
-	int i = 0;
-	int j = 0;
+	if (size_r <= max_len + 1)
+		return 0;
+
+	int i = len1 - 1;
+	int j = len2 - 1;
 	int k = 0;
 
-	while (n1[i] != '\0' || n2[j] != '\0' || carry)
+	while (i >= 0 || j >= 0 || carry)
 	{
-		int num1 = (n1[i] != '\0') ? n1[i] - '0' : 0;
-		int num2 = (n2[j] != '\0') ? n2[j] - '0' : 0;
+		int num1 = (i >= 0) ? n1[i] - '0' : 0;
+		int num2 = (j >= 0) ? n2[j] - '0' : 0;
 
 		int sum = num1 + num2 + carry;
-
-		if (k < size_r - 1)
-		{
-			r[k] = (sum % 10) + '0';
-			k++;
-		}
-		else
-		{
-			return (0);
-		}
-
 		carry = sum / 10;
-
-		if (n1[i] != '\0')
-			i++;
-		if (n2[j] != '\0')
-			j++;
+		r[k++] = (sum % 10) + '0';
+		i--;
+		j--;
 	}
 
 	r[k] = '\0';
 
+	for (int start = 0, end = k - 1; start < end; start++, end--)
+	{
+		char temp = r[start];
+		r[start] = r[end];
+		r[end] = temp;
+	}
+
 	return r;
 }
 
-int main()
+int main(void)
 {
-	char n1[] = "123456789";
-	char n2[] = "987654321";
-	char result[11];
+	char num1[] = "123456789";
+	char num2[] = "987654321";
+	char result[20];
 
-	if (infinite_add(n1, n2, result, sizeof(result)))
+	char *sum = infinite_add(num1, num2, result, sizeof(result));
+	if (sum)
 	{
-		printf("Sum: %s\n", result);
+		printf("Sum: %s\n", sum);
 	}
 	else
 	{
-		printf("Result does not fit in the buffer.\n");
+		printf("Result cannot be stored in the buffer.\n");
 	}
 
 	return 0;
